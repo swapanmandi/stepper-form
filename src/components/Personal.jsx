747d5bbe31e43f43f15fb79e97118a69f256df08
SWAPAN MANDI <swapanmandi5@gmail.com>
2025-02-27 23:58:18 +0530

    enhance Personal component with improved styling and input handling

--- src/components/Personal.jsx
+++ src/components/Personal.jsx
@@ -1,58 +1,76 @@
 import React, { useContext } from "react";
-import {DataContext} from "../dataContextStore/DataContextProvider"
+import { DataContext } from "../dataContextStore/DataContextProvider";
 export default function Personal() {
+  const { personalData, setPersonalData } = useContext(DataContext);
 
-    const {personalData, setPersonalData} = useContext(DataContext)
-    
   const handleOnChange = (e) => {
-setPersonalData({...personalData, [e.target.name]: e.target.value})
+    setPersonalData({ ...personalData, [e.target.name]: e.target.value });
   };
 
-  console.log(personalData.fullName)
   return (
     <div>
-      <form>
-        <fieldset>
+      <form className="bg-slate-700 min-w-[40lvw] min-h-[50lvh] rounded-md p-2">
+        <fieldset className="  flex flex-col justify-start flex-wrap">
           <legend>Personal Info</legend>
-          <label>Full Name:</label>
-          <input
-            type="text"
-            name="fullName"
-            placeholder="Your Name"
-            value={personalData.fullName}
-            onChange={handleOnChange}
-          ></input>
-           <label>Age:</label>
-          <input
-            type="text"
-            name="age"
-            placeholder="Your Age"
-            value={personalData.age}
-            onChange={handleOnChange}
-          ></input>
-           <label>Gender:</label>
-          <input
-            type="text"
-            name="gender"
-            placeholder="Your Gender"
-            value={personalData.gender}
-            onChange={handleOnChange}
-          ></input>
-           <label>Email:</label>
-          <input
-            type="text"
-            name="email"
-            placeholder="Your Email"
-            value={personalData.email}
-            onChange={handleOnChange}
-          ></input> <label>Mobile:</label>
-          <input
-            type="text"
-            name="mobile"
-            placeholder="Your Mobile No"
-            value={personalData.mobile}
-            onChange={handleOnChange}
-          ></input>
+          <div className=" flex justify-start items-center ">
+            <label className=" m-1">Full Name:</label>
+            <input
+              className=" bg-slate-100 p-1 rounded-md text-black m-2"
+              type="text"
+              name="fullName"
+              placeholder="Your Name"
+              value={personalData.fullName}
+              onChange={handleOnChange}
+            ></input>
+          </div>
+          <div className=" flex justify-start items-center ">
+            <label>Age:</label>
+            <input
+              className=" bg-slate-100 p-1 rounded-md text-black m-2"
+              type="number"
+              name="age"
+              placeholder="Your Age"
+              value={personalData.age}
+              onChange={handleOnChange}
+            ></input>
+          </div>
+          <div className=" flex justify-start items-center">
+            <label>Gender:</label>
+            <select
+              className=" bg-slate-100 p-1 rounded-md text-black m-2"
+              name="gender"
+              value={personalData.gender}
+              onChange={handleOnChange}
+            >
+              <option>--Select--</option>
+              <option value="Male"> Male</option>
+              <option value="Female">Female</option>
+              <option value="Other">Other</option>
+            </select>
+          </div>
+          <div className=" flex justify-start items-center">
+            <label>Email:</label>
+            <input
+              className=" bg-slate-100 p-1 rounded-md text-black m-2"
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              value={personalData.email}
+              onChange={handleOnChange}
+            ></input>{" "}
+          </div>
+          <div className=" flex justify-start items-center">
+            <label>Mobile:</label>
+
+            <input
+              className=" bg-slate-100 p-1 rounded-md text-black m-2"
+              type="tel"
+              name="mobile"
+              placeholder="Your Mobile No"
+              value={personalData.mobile}
+              onChange={handleOnChange}
+            ></input>
+          </div>
         </fieldset>
       </form>
     </div>
